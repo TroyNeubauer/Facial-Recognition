a57f9467ae3a9848d43ea70e63ebc8c7f0b964c9
Troy Neubauer <troyneubauer@gmail.com>
2019-08-23 16:24:06 -0400

    Updated imgui

--- Natives/src/Natives.cpp
+++ Natives/src/Natives.cpp
@@ -2,11 +2,82 @@
 
 #include "jni.h"
 #include <stdio.h>
+#include "..\Natives.h"
 
 extern "C" {
+	jclass nativeUtils;
+	JNIEnv* env;
+
 	JNIEXPORT void JNICALL Java_com_mt_face_NativeUtils_setTime(JNIEnv* env, jclass cls, jdouble time)
 	{
-		printf("Native code!\n");
+		
 	}
+
+	JNIEXPORT jdouble JNICALL Java_com_mt_face_NativeUtils_getTime(JNIEnv* env, jclass cls)
+	{
+	}
+
+
+	JNIEXPORT void JNICALL Java_com_mt_face_NativeUtils_play(JNIEnv* env, jclass cls)
+	{
+	}
+
+	JNIEXPORT void JNICALL Java_com_mt_face_NativeUtils_pause(JNIEnv* env, jclass cls)
+	{
+	}
+
+
+	JNIEXPORT jobject JNICALL Java_com_mt_face_NativeUtils_getKnownPeople(JNIEnv* env, jclass cls)
+	{
+	}
+
+	JNIEXPORT jobject JNICALL Java_com_mt_face_NativeUtils_getUnknownPeople(JNIEnv* env, jclass cls)
+	{
+	}
+
+	void* getWindowHandle()
+	{
+		return nullptr;
+	}
+
+	void getClipboard(char* buf, int length)
+	{
+	}
+
+	void setClipboard(char* clipboard)
+	{
+	}
+
+	bool isFocused()
+	{
+		return false;
+	}
+
+	
+	bool isMouseButtonDown()
+	{
+		return false;
+	}
+
+	void getCursorPos(double& x, double& y)
+	{
+	}
+
+	void setCursorPos(double x, double y)
+	{
+	}
+
+	void getWindowSize(double& x, double& y)
+	{
+	}
+
+	double getApplicationTime()
+	{
+		return 0.0;
+	}
+
+
+
+
 
 }
--- Viewer/src/main/java/com/mt/face/natives/NativeUtils.java
+++ Viewer/src/main/java/com/mt/face/natives/NativeUtils.java
@@ -1,15 +1,73 @@
 package com.mt.face.natives;
 
+import java.util.ArrayList;
+
+import com.mt.face.util.MiscUtil;
+
 public class NativeUtils
 {
-
 	static
 	{
 		LibraryUtils.load();
 	}
 
 	public static native void setTime(double time);
+
 	public static native double getTime();
+
+	public static native void play();
+
+	public static native void pause();
+
+	public static native ArrayList<Person> getKnownPeople();
+
+	public static native ArrayList<Person> getUnknownPeople();
 	
+	public static long getWindowHandle() {
+		return 0;
+	}
 	
+	public static String getClipboard() {
+		return "";
+	}
+	
+	public static void setClipboard(String clipboard) {
+		
+	}
+	
+	public static boolean isFocused() {
+		return true;
+	}
+
+	public static boolean isMouseButtonDown(int button)
+	{
+		return false;
+	}
+
+	public static void getCursorPos(long x, long y)
+	{
+		MiscUtil.getUnsafe().putDouble(x, 0.0);
+		MiscUtil.getUnsafe().putDouble(y, 0.0);
+	}
+
+	public static void setCursorPos(double x, double y)
+	{
+
+	}
+
+	public static void getWindowSize(long x, long y)
+	{
+		MiscUtil.getUnsafe().putDouble(x, 0.0);
+		MiscUtil.getUnsafe().putDouble(y, 0.0);
+	}
+
+	private static long start = -1;
+
+	public static double getApplicationTime()
+	{
+		if (start == -1)
+			start = System.nanoTime();
+		return (System.nanoTime() - start) / 1000000000.0;
+	}
+
 }
